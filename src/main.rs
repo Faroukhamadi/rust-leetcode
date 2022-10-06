932de7e3081c5d6dfc7f690b5dc4c45ccf97ef0d
Faroukhamadi <faroukhamadi1@outlook.com>
2022-10-06 22:47:39 +0100

    add solutions

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
-mod thing;
-use thing::hello;
+mod reverse_linked_list_solution;
+mod two_sum_solution;
+use reverse_linked_list_solution::{reverse_list, ListNode};
+use two_sum_solution::two_sum;
 
 fn main() {
-    hello();
+    let v = two_sum(vec![3, 2, 4], 6);
+    reverse_list(Option::Some(Box::new(ListNode { val: 2, next: None })));
 }
