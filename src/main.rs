6c1eed3bc7c725fcfade687cb4610d536d00b2cc
Faroukhamadi <faroukhamadi1@outlook.com>
2022-10-20 21:07:30 +0100

    add solution for 3

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,10 @@
 mod contains_duplicate_solution;
 mod group_anagrams_solution;
+mod longest_substring_without_repeating_characters_solution;
 
-fn main() {}
+fn main() {
+    let s: String = String::from("hello world");
+    for c in s.into_bytes() {
+        println!("c is: {}", c);
+    }
+}
